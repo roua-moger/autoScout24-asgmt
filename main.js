6da8e25a4958b4ac7074875dacdfd0fda15a03b2
Rowaa <rowaa.almogheer@gmail.com>
2021-01-21 09:08:14 +0100

    read second csv file

--- main.js
+++ main.js
@@ -2,11 +2,23 @@
 
 const csv = require('csvtojson');
 const fs = require('fs');
+
+//read listings.csv
 const converter = csv()
 .fromFile('./listings.csv')
 .then((json) => {
     json.forEach((row) => {
      
        // console.log(row);
+    });
+})
+
+//read contacts.csv
+const convert = csv()
+.fromFile('./contacts.csv')
+.then((json) => {
+    json.forEach((row) => {
+     
+        console.log(row);
     });
 })
