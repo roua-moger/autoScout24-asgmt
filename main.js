a3f98ee23ba3beec0b017d68da2cc227d03582e3
Rowaa <rowaa.almogheer@gmail.com>
2021-01-21 10:39:15 +0100

    cont the perc + creat percTable.json

--- main.js
+++ main.js
@@ -20,7 +20,14 @@ let vw = [];
 let fiat = [];
 let mercedesBenz = [];
 //var for make car counting 
-
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+let count4 = 0;
+let count5 = 0;
+let count6 = 0;
+let count7 = 0;
+let count8 = 0;
 
 const csv = require('csvtojson');
 const fs = require('fs');
@@ -73,13 +80,14 @@ const converter = csv()
     for(let i = 0; i < private.length ; i++){
         sumP += parseInt(private[i]);
     }
-    for (let j = 0 ; j < dealer.length ; j++){
-        sumD += parseInt(dealer[j]);
+    for (let i = 0 ; i < dealer.length ; i++){
+        sumD += parseInt(dealer[i]);
     }
-    for(let c = 0 ; c < other.length ; c++){
-        sumO += parseInt(other[c]);
+    for(let i = 0 ; i < other.length ; i++){
+        sumO += parseInt(other[i]);
     }
-        //calculate the average
+
+    //calculate the average
 
     // let pAvg = sumP / private.length;
     // let dAvg = sumD / dealer.length;
@@ -106,8 +114,6 @@ const converter = csv()
             average : '€ ' + (sumO / other.length).toFixed(0).replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'),
         },
     ]
-
-   
 };
     //convert to json file
     let data = JSON.stringify(avgTable, null, 2);
@@ -117,17 +123,91 @@ const converter = csv()
 
     //test wich cars are in make and howmany
     var counter = {};
-    makeList.forEach(function(r) { counter[r] = (counter[r]||0) + 1;});
+    makeList.forEach((r) =>
+     { counter[r] = (counter[r]||0) + 1;
+    });
     console.log(counter);
 
-     for(let m = 0; m < makeList.length ; m++){  
-        if(newMakeList.indexOf(makeList[m]) === -1){
-            newMakeList.push(makeList[m]);
+     for(let i = 0; i < makeList.length ; i++){  
+        if(newMakeList.indexOf(makeList[i]) === -1){
+            newMakeList.push(makeList[i]);
         }; 
     }
      console.log(newMakeList);
 
-})
+     //calculate the percentual
+     for(let i = 0; i < audi.length ; i++){
+         if(audi[i]) count1++
+     }
+     for(let i = 0; i < mazda.length; i++){
+         if(mazda[i]) count2++
+     }
+     for(let i = 0; i < bwm.length; i++){
+        if(bwm[i]) count3++
+    }
+    for(let i = 0; i < toyota.length; i++){
+        if(toyota[i]) count4++
+    }
+    for(let i = 0; i < renanlt.length; i++){
+        if(renanlt[i]) count5++
+    }
+    for(let i = 0; i < vw.length; i++){
+        if(vw[i]) count6++
+    }
+    for(let i = 0; i < fiat.length; i++){
+        if(fiat[i]) count7++
+    }
+    for(let i = 0; i < mercedesBenz.length; i++){
+        if(mercedesBenz[i]) count8++
+    }
+
+    //test how to count the percentual
+    let audiPerc = count1 / makeList.length * 100;
+    console.log(audiPerc);
+
+    //creat a new json for the average data
+    let percTable = {
+        percList : [
+            {
+                make: newMakeList[0],
+                pecentual : (count1 / makeList.length * 100).toFixed(0) + ' %',
+            },
+            {
+                make: newMakeList[1],
+                pecentual : (count2 / makeList.length * 100).toFixed(0) + ' %',
+            },
+            {
+                make: newMakeList[2],
+                pecentual : (count3 / makeList.length * 100).toFixed(0) + ' %',
+            },
+            {
+                make: newMakeList[3],
+                pecentual : (count4 / makeList.length * 100).toFixed(0) + ' %',
+            },
+            {
+                make: newMakeList[4],
+                pecentual : (count5 / makeList.length * 100).toFixed(0) + ' %',
+            },
+            {
+                make: newMakeList[5],
+                pecentual : (count6 / makeList.length * 100).toFixed(0) + ' %',
+            },
+            {
+                make: newMakeList[6],
+                pecentual : (count7 / makeList.length * 100).toFixed(0) + ' %',
+            },
+            {
+                make: newMakeList[7],
+                pecentual : (count8 / makeList.length * 100).toFixed(0) + ' %',
+            }
+        ]
+    };
+
+    //convert to json file
+    let dataPerc = JSON.stringify(percTable, null, 2);
+    fs.writeFileSync('percTable.json', dataPerc);
+    console.log(percTable);
+});
 
 //read contacts.csv
 const convert = csv()
